c146888fef12efd4f8316f132f8a62078123f5a2
Logan Anderson <landerson2@upei.ca>
2019-11-10 20:50:29 -0400

    added some contents

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var aboutRouter = require('./routes/about');
+let addNoteRouter = require('./routes/addNote');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
+app.use('/add-note',addNoteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
