60f45f12a84af1e7b9a4e923f148f1a17f3812a4
logan-anderson <landerson2@upei.ca>
2019-11-11 15:31:42 -0400

    added dummy content for testing

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index');
+router.get('/', function (req, res, next) {
+  var n = [
+    {
+      'title': "Logans Note",
+      'content': ['Note one', 'note two'],
+    },
+    {
+      'title': "Maddie Note",
+      'content': ['Note one', 'note two'],
+    },
+    {
+      'title': "Katie Note",
+      'content': ['Note one', 'note two'],
+    },
+  ]
+  res.render('pages/index', { notes: n });
 });
 
 module.exports = router;
