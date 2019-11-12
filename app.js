cd15025cc96b15f697109e7b7bcae109018a0ad6
logan-anderson <landerson2@upei.ca>
2019-11-12 15:44:54 -0400

    added deleteing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var aboutRouter = require('./routes/about');
-let addNoteRouter = require('./routes/addNote');
+let noteRouter = require('./routes/note');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
-app.use('/add-note', addNoteRouter);
+app.use('/note', noteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
