a2805986e4c885555cae48d5ff1a8bf8c4e00504
logan-anderson <landerson2@upei.ca>
2019-11-12 19:14:33 -0400

    added feature to update the lists

---
+++ public/js/editNote.js
@@ -0,0 +1,48 @@
+hiddenFormNotes = document.querySelector("#note-list-edit");
+function add_item_edit() {
+    //set the hidden title form value
+    document.getElementById("title-form-edit").value = document.getElementById('title-input-edit').value;
+
+    let cols = document.querySelector(".col-edit");
+    let itemRow = document.querySelector(".list-items-row-edit");
+
+    // let allRows = document.querySelectorAll(".list-items-row");
+    // let lastRow = allRows[allRows.length-1];
+    // update as we go
+    // hiddenFormNotes.value = hiddenFormNotes.value + ',' + lastRow.childNodes[1].childNodes[1].value;
+
+    // or update everything
+    hiddenFormNotes.value = JSON.stringify(getData_edit());
+
+    let itemRowNew = itemRow.cloneNode(true);
+    itemRowNew.childNodes[1].childNodes[1].value = "";
+    cols.appendChild(itemRowNew);
+    setupListeners_edit();
+}
+
+function setupListeners_edit() {
+    let buttons = document.querySelectorAll('.add-item-btn-edit');
+    buttons.forEach((x) => {
+        x.addEventListener("click", add_item_edit);
+    });
+}
+function getData_edit() {
+    let data = [];
+    document.querySelectorAll(".list-items-row-edit").forEach((x) => {
+        data.push(x.childNodes[1].childNodes[1].value);
+    });
+    return data;
+}
+setupListeners_edit();
+//update form when the save button is clicked
+document.querySelector(".btn-edit-save").addEventListener("click", (event) => {
+    hiddenFormNotes.value = JSON.stringify(getData_edit());
+    document.getElementById("title-form-edit").value = document.getElementById('title-input-edit').value;
+});
+
+//update on key presses if needed
+// document.onkeypress = function (e) {
+//     e = e || window.event;
+//     console.log("key pressed")
+// };
+
--- routes/note.js
+++ routes/note.js
@@ -1,37 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-
+mongoose.set('useFindAndModify', false);
 var Note = mongoose.model('Note');
 
-router.get('/', function (req, res, next) {
-    // creat a note in the database
-    Note.create({
-        note_id: 2,
-        list_content: ["this is a note", "so is this"],
-        title: "DB Note",
-    }, (err, note) => {
-        //call back function
-        if (err) {
-            console.log("Something went wrong");
-            res.send("Server error");
-        } else {
-            console.log("We saved a note to the db");
-            // console.log(note);
-            // get all the notes
-            Note.find({}, (err, nts) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    //array of notes
-                    console.log(nts);
-                    ///send back the main page
-                    res.render('pages/index', { notes: nts });
-                }
-            });
-        }
-    });
-});
+// router.get('/', function (req, res, next) {
+//     // creat a note in the database
+//     Note.create({
+//         note_id: 2,
+//         list_content: ["this is a note", "so is this"],
+//         title: "DB Note",
+//     }, (err, note) => {
+//         //call back function
+//         if (err) {
+//             console.log("Something went wrong");
+//             res.send("Server error");
+//         } else {
+//             console.log("We saved a note to the db");
+//             // console.log(note);
+//             // get all the notes
+//             Note.find({}, (err, nts) => {
+//                 if (err) {
+//                     console.log(err);
+//                 } else {
+//                     //array of notes
+//                     console.log(nts);
+//                     ///send back the main page
+//                     res.render('pages/index', { notes: nts });
+//                 }
+//             });
+//         }
+//     });
+// });
 router.post('/add', function (req, res, next) {
     console.log("we got a post request");
     console.log(req.body.title);
@@ -57,8 +57,8 @@ router.post('/delete/:id', function (req, res, next) {
     console.log("we got a post request");
     var id = req.params.id;
     console.log(id);
-    Note.deleteOne({_id:id},(err)=>{
-        if(err) {
+    Note.deleteOne({ _id: id }, (err) => {
+        if (err) {
             console.log("did not delete");
             res.redirect('/');
         } else {
@@ -68,5 +68,39 @@ router.post('/delete/:id', function (req, res, next) {
     });
     // res.redirect('/');
 });
+router.get('/edit/:id', function (req, res, next) {
+    console.log("the note page");
+    var id = req.params.id;
+    Note.findOne({ _id: id }, (err, nt) => {
+        if (err) {
+            console.log("did not find a note");
+            res.redirect('/');
+        } else {
+            console.log("found note");
+            console.log(nt);
+            console.log(typeof (nt));
+            res.render('pages/editNote', { note: nt });
+            // res.redirect("/");
+        }
+    });
+});
+router.post('/edit/:id', function (req, res, next) {
+    console.log("we got a post request");
+    console.log(req.body.title);
+    var newNotes = JSON.parse(req.body.notes)
+    console.log(newNotes);
 
+    var id = req.params.id;
+    var newTitle = req.body.title;
+    console.log(id);
+
+    Note.findOneAndUpdate({ _id: id }, { list_content: newNotes, title: newTitle }, { upsert: true }, function (err, doc) {
+        if (err) {
+            console.log("error in updating");
+        } else {
+            console.log("updated");
+            res.redirect('/');
+        }
+    });
+});
 module.exports = router;
