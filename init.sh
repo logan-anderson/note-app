60acf035231fa6009e3bcdb5ef0df849afc109b4
logan-anderson <landerson2@upei.ca>
2019-12-23 16:26:09 -0400

    worked on init script for github actions

--- init.sh
+++ init.sh
@@ -1,3 +1,17 @@
 mkdir -p credentials
-touch credentials/credentials.js
-cp credentials.js.template  credentials/credentials.js
+# touch credentials/credentials.js
+cat  << EOF > temp.cred
+module.exports = {
+    local: false,
+    useUri: true,
+    uri: '$mongo_db_URI',
+    userName: '',
+    password: '',
+    clusterName: '',
+    dev: '',
+    prod: '',
+} 
+EOF
+
+cp temp.cred ./credentials/credentials.js
+rm temp.cred
