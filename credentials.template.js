7682ae7fa7a3c4ebc07dc633ae8bad0160ad927d
Logan Anderson <landerson2@upei.ca>
2019-12-23 21:08:44 -0400

    rename file

---
+++ credentials.template.js
@@ -0,0 +1,12 @@
+module.exports = {
+  local: false,
+  useUri: false,
+  uri: '',
+  userName: '',
+  password: '',
+  clusterName: '',
+  dev: 'dev',
+  prod: 'prod',
+  devUser: 'admin@admin',
+  devPass: 'admin',
+};
