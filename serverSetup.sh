10a602726177fe92ba1de9539dd495ae426e9a40
Logan Anderson <landerson2@upei.ca>
2020-01-07 21:54:02 -0400

    added basic server script

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,4 +1,28 @@
-
-# install node
+#!/bin/sh
+# Update Packages
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
+
+# SERVER SETUP
+# install all the packages we need
+sudo apt install nodejs npm  nginx build-essential -y
+
+# test to make sure node and npm where installed
+if ! node -v ; then
+    echo Nodejs was not installed
+    exit 1
+fi
+
+if ! npm -v ; then
+    echo npm was not installed
+    exit 1 
+fi
+
+# setup Nginx
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'OpenSSH'
+sudo ufw enable
+
+
+
+# setup 
